371bf46081342aeca2e7d2c5a12e31ada8d087bf
planetblix <binodbista@gmail.com>
2014-01-29 19:13:36 +0000

    Add ex10.py

--- ex10.py
+++ ex10.py
@@ -32,14 +32,44 @@ doublequote = "\""
 print "This is also a double-quote: %s" % doublequote
 print "This is also a double-quote: %r" % doublequote
 
-print "This is a ascii-bell: \a"
+print "This is a ascii-bell (BEL): \a"
 asciibell = "\a"
 #Note: This line DOES sound a bell.
-print "This is also a ascii-bell: %s" % asciibell
+print "This is also a ascii bell: %s" % asciibell
 #Note: This line doesn't sound a bell.
-print "This is also a ascii-bell: %r" % asciibell
+print "This is also a ascii bell: %r" % asciibell
+#Interesting, the string 'test' prints 'test'.
+print "This is a ascii bell in the middle of a string: te%sst" % asciibell
 
+print "This is a ascii backspace (BS): \b"
+asciibackspace = "\b"
+print "This is a ascii backspace: %s" % asciibackspace
+print "This is a ascii backspace: %r" % asciibackspace
+#Interesting, the 'e' disappears 
+print "This is a ascii backspace: te%sst" % asciibackspace
 
+print "This is a ascii formfeed (FF): \f"
+asciiformfeed = "\f"
+print "This is a ascii formfeed: %s" % asciiformfeed
+print "This is a ascii formfeed: %r" % asciiformfeed
+print "This is a ascii formfeed: te%sst" % asciiformfeed
+
+print "This is a ascii linefeed (LF): \n"
+asciilinefeed = "\n"
+
+#Compare ''' with """ - notice the double and single quotes
+print '''
+This is a double-quote: "%s" 
+This is a ascii-bell: "%s"
+This is a ascii-formfeed: "%s"
+This is a ascii-linefeed: "%s"
+''' % (doublequote,asciibell, asciiformfeed, asciilinefeed)
+print """
+This is a double-quote: "%s" 
+This is a ascii-bell: '%s'
+This is a ascii-formfeed: '%s'
+This is a ascii-linefeed: '%s'
+""" % (doublequote,asciibell, asciiformfeed, asciilinefeed)
 
 
 
