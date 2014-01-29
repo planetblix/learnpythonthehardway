640cf6410351567b3b94cda2dfff2b7846cadb2d
planetblix <binodbista@gmail.com>
2014-01-29 20:21:56 +0000

    Add ex11.py

---
+++ ex11.py
@@ -0,0 +1,15 @@
+#!/bin/python
+
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+#Notes:
+#The comma at the end of the print line doesn't end the line with a newline character.
+
+
