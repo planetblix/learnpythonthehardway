b78e4f24e75d4c299ade8e6e143215ac2b62ebc7
planetblix <binodbista@gmail.com>
2014-06-22 20:20:35 +0100

    Adding ex35.py

--- ex35.py
+++ ex35.py
@@ -1,10 +1,14 @@
 #/bin/python
 
+#Q5.The gold_room has a weird way of getting you to type a number. What are all the bugs in this way of doing it? Can you make it better than just checking if "1" or "0" are in the number? Look at how int() works for clues.
+#A5. The problem here is, if you type in a number less than 50 that doesn't include 1 or 0 e.g 42, then that number isn't stored in how_much and therefore the comparison at 'if how_much < 50' is resolved as false. Here it's best to have a range, than looking for '0' or '1'.
+
 def gold_room():
     print "This room is full of gold. How much do you take?"
 
     next = raw_input("> ")
-    if "0" in next or "1" in next:
+    #if "0" in next or "1" in next:
+    if next >= 50:
         how_much = int(next)
     else:
         dead("Man, learn to type a number.")
